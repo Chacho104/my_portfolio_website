d808f26ca94f3dcc5aceea7f206c7501bcebeaee
Churchil Owino <chachowino75@gmail.com>
2022-10-21 09:11:53 +0300

    Create and export the Banner component

---
+++ src/Components/Banner/index.js
@@ -0,0 +1,20 @@
+import './index.scss';
+
+const Banner = () => {
+    return (
+        <div className='banner'>
+            <div className='intro'>
+                <span>Hello, nice to meet you!</span> 
+                <h1>Churchil Owino here...</h1>
+                <p>I'm an enthusiastic, customer-driven fullstack software engineer, ready 
+                    <br /> to turn yout brilliant ideas into great solutions for the digital 
+                    space. I'm <br /> currently focused on building responsive and interactive 
+                    UIs from scratch, <br /> ensuring you can actualize just about any idea.
+                </p>
+                <button>Tour my Showroom</button>
+            </div>
+        </div>
+    )
+}
+
+export default Banner
