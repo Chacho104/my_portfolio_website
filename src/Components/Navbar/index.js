c8b086205ad97756f556ccd23ffb511dd397959c
Churchil Owino <chachowino75@gmail.com>
2022-10-21 09:08:37 +0300

    Create and export Navbar component

---
+++ src/Components/Navbar/index.js
@@ -0,0 +1,17 @@
+import { NavLink } from 'react-router-dom';
+import './index.scss';
+
+const Navbar = () => {
+    return (
+        <div className='navbar'>
+            <nav>
+                <NavLink className='linkItem'>Home</NavLink>
+                <NavLink className='linkItem'>A Little About Me</NavLink>
+                <NavLink className='linkItem'>The Showroom</NavLink>
+                <NavLink className='linkItem'>Actualize that Idea...</NavLink>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
