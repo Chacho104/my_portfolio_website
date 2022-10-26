e848041f3ebfc2e775daccecf0964e459b28c620
Churchil Owino <chachowino75@gmail.com>
2022-10-26 10:17:17 +0300

    Build the contact component

--- src/Components/Contact/index.js
+++ src/Components/Contact/index.js
@@ -4,6 +4,21 @@ const Contact = () => {
     return (
         <div className='contact'>
             <h1>Contact</h1>
+            <p>Ready to take up your space on the web? Drop me a message</p>
+
+            <div className='contactForm'>
+                <form>
+                   <input type='text' name='name' placeholder='Name' required />
+                   <br />
+                   <input type='email' name='email' placeholder='Email' required />
+                   <br />
+                   <input type='Subject' name='subject' placeholder='Subject' required />
+                   <br />
+                   <textarea placeholder='Message' name='message' required></textarea>
+                   <br />
+                   <button>Send</button>
+                </form>
+            </div>
         </div>
     );
 }
