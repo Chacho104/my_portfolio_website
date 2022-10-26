c3627813c23df29788a8233383d7daefcf9c14d1
Churchil Owino <chachowino75@gmail.com>
2022-10-26 10:19:21 +0300

    Revise the about paragraph Remove the welcome paragraph

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -6,10 +6,13 @@ const About = () => {
         <div className='mePar'>
             <img src={ProfilePic} alt='Churchil Owino' />
             <p> 
-                My name is Churchil Owino, a full-stack software engineer based 
+                My name is Churchil Owino, a junior full-stack software engineer based 
                 in Nairobi, Kenya. I am all about turning brilliant ideas into digital 
-                solutions that solve real world problems for individuals and businesses.
-                I believe no idea is impossible as long as we leverage the right technology, 
+                solutions that solve real world problems faced by individuals or 
+                businesses.
+            </p>
+            <p>
+                I believe no idea is impossible as long as we leverage the right technology 
                 and summon our most creative faculties. If you can think of it, then it can 
                 be created, and I am ready to do just that. 
             </p>
@@ -17,7 +20,6 @@ const About = () => {
                 Feel free to contact me and share your wildest ideas, and we will build 
                 something amazing using the tools and technologies alongside!
             </p>
-            <p>Most welcome!</p>
         </div>
     );
 }
